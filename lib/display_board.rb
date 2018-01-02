748ee702325a989139d6f41bf4bb2cdc08bf9fdf
Justin K <justin-kim1@hotmail.com>
2018-01-02 19:45:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,4 +6,14 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 puts board
+=======
+def display_board(board, rows)
+  puts "      |       |     "
+  puts "-----------"
+  puts "      |       |     "
+  puts "-----------"
+  puts "      |       |     "
+puts ${rows[]}
+puts board
+>>>>>>> 18ad12f75bb3319afd1bd0fd1cec31a8f19dfb78
 end
